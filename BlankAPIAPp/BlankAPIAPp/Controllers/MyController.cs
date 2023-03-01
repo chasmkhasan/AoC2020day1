a8d3cf4d98b378af5a99547843ad1906d75182c2
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-03-01 12:06:55 +0100

    first commit

---
+++ BlankAPIAPp/BlankAPIAPp/Controllers/MyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlankAPIAPp.Controllers
+{
+    [ApiController]
+    [Route ("api/my")]
+    public class MyController : ControllerBase
+    {
+        //HTTP verb: HTTPGet, HTTPPost,HTTPPut, HTTPPatch, HTTPDelete
+                        
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new {id = 1, Name = "MK Hasan" },
+                    new {id = 2, Name = "Homaiyen Kabir" },
+                    new {id = 3, Name = "Ruhul Amin" },
+                    new {id = 4, Name = "Rashedul Islam" },
+                    new {id = 5, Name = "Topu Bishaw" },
+                    new {id = 6, Name = "Shahrier Kabir" },
+                    
+                }
+                );
+        }
+    }
+}
---
+++ BlankAPIAPp/BlankAPIAPp/Program.cs
@@ -0,0 +1,50 @@
+namespace BlankAPIAPp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+          
+
+            // below for individual information
+            /*
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("This is my end point");
+            //});*/
+
+            app.Run();
+        }
+    }
+}
